1864af7327adeb72a69e792a76ec1daab978d7a1
Luke Hargraves <lhargraves@spartaglobal.co>
2016-11-15 13:47:45 +0000

    Added master function. Tested green

--- person.rb
+++ person.rb
@@ -1,3 +1,69 @@
 def add_numbers(a, b)
 	a + b
 end
+
+def subtract_numbers(a, b)
+	return a - b
+end
+
+def add_array_numbers(a)
+	sum = 0
+	a.each {|i| sum += i}
+	sum
+end
+
+def subtract_array_numbers(a)
+	sum = a[0]
+	a.each {|i| sum -= i}
+	return sum
+end
+
+def add_array_numbers1(a)
+	a.reduce(0, :+)
+end
+
+def multiply_numbers(a, b)
+	a*b
+end
+
+def divide_numbers(a, b)
+	if b > 0
+		a/b
+	else
+		"inf"
+	end
+end
+
+def multiply_array_numbers(a)
+	a.reduce(1, :*)
+end
+
+def raise2power(a, b)
+	a ** b
+end
+
+def factorial(a)
+    (1..a).inject(:*) || 1
+end
+
+def smooth_operator(a)
+	if a[:function] == 'add'
+		add_array_numbers(a[:data])
+	elsif a[:function] == 'subtract'
+		subtract_numbers(a[:data][0], a[:data][1])
+	elsif a[:function] == 'divide'
+		divide_numbers(a[:data][0], a[:data][1])
+	elsif a[:function] == 'multiply'
+		multiply_array_numbers(a[:data])
+	elsif a[:function] == 'indices'
+		raise2power(a[:data][0], a[:data][1])
+	elsif a[:function] == 'factorial'
+		factorial(a[:data][0])
+	else
+		return "That function is not allowed"		
+	end
+end
+
+
+
+
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,5 +1,63 @@
-describe 'adding machine' do 
+describe 'arithmetic machine' do 
 	it 'should add two numbers' do
 		expect(add_numbers(5, 6)).to eq 11
+	end
+
+	it 'should subtract two numbers' do
+		expect(subtract_numbers(5, 3)).to eq 2
+	end 
+
+	it 'should add an array of numbers' do
+		expect(add_array_numbers([5, 6, 10])).to eq 21
+	end
+
+	it 'should return 0 for an empty array' do
+		expect(add_array_numbers([])).to eq 0
+	end
+
+	it 'should multiply two numbers' do
+		expect(multiply_numbers(5, 3)).to eq 15
+	end
+
+	it 'should divide two numbers' do
+		expect(divide_numbers(10, 2)).to eq 5
+	end
+
+	it 'should multiply an array of numbers' do
+		expect(multiply_array_numbers([1, 2, 3])).to eq 6
+	end
+
+	it 'should not divide by 0' do
+		expect(divide_numbers(6,0)).to eq 'inf'
+	end
+
+	it 'should raise an input to a power specified' do
+		expect(raise2power(2,4)).to eq 16
+	end
+
+	it 'should return the factorial of an input' do
+		expect(factorial(5)).to eq 120
+	end
+
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'add', data: [1, 3, 5] })).to eq 9
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'divide', data: [6, 2] })).to eq 3
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'multiply', data: [3,5]})).to eq 15
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'indices', data: [2, 4]})).to eq 16
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'factorial', data: [5] })).to eq 120
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'subtract', data: [5,2]})).to eq 3
+	end
+	it 'should return the result for whichever operator is required' do
+		expect(smooth_operator({function: 'nothing', data: [1,2,3]})).to eq 'That function is not allowed'
 	end
 end
