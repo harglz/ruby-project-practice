76f88193bab597b3dd481d8f2fc49413f827124e
Luke Hargraves <lhargraves@spartaglobal.co>
2016-11-15 15:36:43 +0000

    Wrote Person Class. Tested Green

---
+++ person/person.rb
@@ -0,0 +1,50 @@
+class Person
+	#constructor takes Person.new and runs this initialise method
+	def initialize
+		#instance variables @ allow variables to be used anywhere within class.
+		@height = 0.0
+		@top_color = nil
+		@hair_color = nil
+	end
+
+
+	def set_hair_color(color)
+		@hair_color = color
+	end
+
+
+	def get_hair_color
+		return @hair_color
+	end
+
+
+	def set_top_color(color)
+		@top_color = color
+	end
+
+
+	def get_top_color
+		return @top_color
+	end
+
+
+	def set_height(height)
+		@height = height
+	end
+
+
+	def get_height
+		return @height
+	end
+
+
+	def dance
+		return "I'm Dancing!"
+	end
+
+
+	def sleep
+		return "ZzzZzzZzzZzzZzz"
+	end
+
+end
--- person/spec/person_spec.rb
+++ person/spec/person_spec.rb
@@ -1,63 +1,37 @@
-describe 'arithmetic machine' do 
-	it 'should add two numbers' do
-		expect(add_numbers(5, 6)).to eq 11
+describe Person do 
+	it 'should initialize' do
+		bob = Person.new #Create a new instance of the Person class, and put it in the variable
+		expect(bob.class).to eq Person
+		expect(bob.get_hair_color).to eq nil
+		expect(bob.get_top_color).to eq nil
+		expect(bob.get_height).to eq 0.0
 	end
 
-	it 'should subtract two numbers' do
-		expect(subtract_numbers(5, 3)).to eq 2
-	end 
-
-	it 'should add an array of numbers' do
-		expect(add_array_numbers([5, 6, 10])).to eq 21
+	it 'should let us set the hair color' do
+		bob = Person.new
+		bob.set_hair_color 'brown'
+		expect(bob.get_hair_color).to eq 'brown'
 	end
 
-	it 'should return 0 for an empty array' do
-		expect(add_array_numbers([])).to eq 0
+	it 'should let us set the top color' do
+		bob = Person.new
+		bob.set_top_color 'black'
+		expect(bob.get_top_color).to eq 'black'
 	end
 
-	it 'should multiply two numbers' do
-		expect(multiply_numbers(5, 3)).to eq 15
+	it 'should let us set the height' do
+		bob = Person.new
+		bob.set_height 6.5
+		expect(bob.get_height).to eq 6.5
 	end
 
-	it 'should divide two numbers' do
-		expect(divide_numbers(10, 2)).to eq 5
+	it 'should be able to dance' do
+		bob = Person.new
+		expect(bob.dance).to eq "I'm Dancing!"
 	end
 
-	it 'should multiply an array of numbers' do
-		expect(multiply_array_numbers([1, 2, 3])).to eq 6
-	end
-
-	it 'should not divide by 0' do
-		expect(divide_numbers(6,0)).to eq 'inf'
-	end
-
-	it 'should raise an input to a power specified' do
-		expect(raise2power(2,4)).to eq 16
-	end
-
-	it 'should return the factorial of an input' do
-		expect(factorial(5)).to eq 120
-	end
-
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'add', data: [1, 3, 5] })).to eq 9
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'divide', data: [6, 2] })).to eq 3
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'multiply', data: [3,5]})).to eq 15
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'indices', data: [2, 4]})).to eq 16
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'factorial', data: [5] })).to eq 120
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'subtract', data: [5,2]})).to eq 3
-	end
-	it 'should return the result for whichever operator is required' do
-		expect(smooth_operator({function: 'nothing', data: [1,2,3]})).to eq 'That function is not allowed'
+	it 'should be able to sleep' do
+		bob = Person.new
+		expect(bob.sleep).to eq "ZzzZzzZzzZzzZzz"
 	end
 end
