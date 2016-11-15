ed44b52affda6e5d169566b37c82b21820515f23
Luke Hargraves <lhargraves@spartaglobal.co>
2016-11-15 15:38:56 +0000

    Refactored person.rb

--- person/person.rb
+++ person/person.rb
@@ -7,44 +7,35 @@ class Person
 		@hair_color = nil
 	end
 
-
 	def set_hair_color(color)
 		@hair_color = color
 	end
-
 
 	def get_hair_color
 		return @hair_color
 	end
 
-
 	def set_top_color(color)
 		@top_color = color
 	end
-
 
 	def get_top_color
 		return @top_color
 	end
 
-
 	def set_height(height)
 		@height = height
 	end
-
 
 	def get_height
 		return @height
 	end
 
-
 	def dance
 		return "I'm Dancing!"
 	end
 
-
 	def sleep
 		return "ZzzZzzZzzZzzZzz"
 	end
-
 end
