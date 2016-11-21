3d2d071b5feaffe3e20849c00ac0563310d1dd98
Luke Hargraves <lhargraves@spartaglobal.co>
2016-11-21 10:37:42 +0000

    Tests Green and New Search Functionality added

--- todos/spec/todo_spec.rb
+++ todos/spec/todo_spec.rb
@@ -1,35 +1,51 @@
 require './todo.rb'
 
 describe 'Todos app' do
-  
+
+  before(:each) { Todo.class_variable_set :@@list, [] }
+
   it 'should initialise' do
     todo = Todo.new('Remember the Milk', '18 November 2016')
     expect(todo.title).to eq 'Remember the Milk'
-    expect(todo.due_date.class).to eq Date
+    expect(todo.due_date).to eq '18 November 2016'
+  end
+
+  it 'should initialise' do
+    todo = Todo.new('Remember the Milk')
+    expect(todo.title).to eq 'Remember the Milk'
+    expect(todo.due_date).to eq Date.today.strftime('%d %B %Y')
   end
 
   it 'should return an array of todos' do
     Todo.new('Remember the Milk', '18 November 2016')
     expect(Todo.all.class).to eq Array
-    expect(Todo.all.last).to eq 'Remember the Milk'
+    expect(Todo.last).to eq ['Remember the Milk', '18 November 2016']
     Todo.new('Second todo', '18 November 2016')
     expect(Todo.all.class).to eq Array
-    expect(Todo.all.last).to eq 'Second todo'
+    expect(Todo.last).to eq ['Second todo', '18 November 2016']
   end
 
   it 'should return the last todo from the list' do
     Todo.new('First', '18 November 2016')
     Todo.new('Remember the Milk', '18 November 2016')
     Todo.new('Last', '18 November 2016')
-    expect(Todo.last.class).to eq Hash
+    expect(Todo.last).to eq ['Last', '18 November 2016']
   end
 
   it 'should return the todos that contain words' do
     todo = Todo.new('First todo', '18 November 2016')
     todo = Todo.new('Remember the Milk', '18 November 2016')
     todo = Todo.new('Last todo', '18 November 2016')
-    expect(Todo.find contains: 'todo').to eq ['First todo', 'Last todo']
-    expect(Todo.find exactly: 'Remember the Milk').to eq ['Remember the Milk']
+    expect(Todo.find contains: 'todo').to eq [['First todo', '18 November 2016'], ['Last todo', '18 November 2016']]
+    expect(Todo.find exactly: 'Remember the Milk').to eq [['Remember the Milk', '18 November 2016']]
+  end
+
+  it 'should return the todos that contain dates' do
+    todo = Todo.new('First todo', '18 November 2016')
+    todo = Todo.new('Remember the Milk', '19 November 2016')
+    todo = Todo.new('Last todo', '18 November 2016')
+    expect(Todo.find date_contains: '18').to eq [['First todo', '18 November 2016'], ['Last todo', '18 November 2016']]
+    expect(Todo.find date_exactly: '19 November 2016').to eq [['Remember the Milk', '19 November 2016']]
   end
   
   it 'should set and get the due date' do
@@ -39,7 +55,7 @@ describe 'Todos app' do
     expect(todo.due_date).to eq '20 November 2016'
   end
 
-  it 'should set and get the due date' do
+  it 'should set and get the title' do
     todo = Todo.new('Test something', '18 November 2016')
     expect(todo.title).to eq 'Test something'
     todo.title = 'New todo'
---
+++ todos/todo.rb
@@ -0,0 +1,66 @@
+require 'date'
+
+class Todo
+  attr_accessor :title
+  attr_reader :due_date
+
+  @@list = []
+
+  def initialize(title, due_date = Date.today.strftime('%d %B %Y'))
+    @title = title
+    @due_date = due_date
+    date(@due_date)
+    @@list << [@title, @due_date]
+  end
+
+  def date(due_date)
+    date = Date.parse(due_date)
+    @due_date = date.strftime('%d %B %Y')
+  end
+
+  def due_date=(due_date)
+    date(due_date)
+  end
+
+  def self.all
+    return @@list
+  end
+
+  def self.last
+    return @@list.last
+  end
+
+  def self.find(params)
+    print_list = []
+    if params[:contains] 
+      search_term = params[:contains]
+      @@list.each do |title, due_date|
+        if title.include? search_term
+          print_list << [title, due_date]
+        end
+      end
+    elsif params[:exactly]
+      string = params[:exactly]
+      @@list.each do |title, due_date|
+        if string == title
+          print_list << [title, due_date]
+        end
+      end
+    elsif params[:date_contains]
+      search_term = params[:date_contains]
+      @@list.each do |title, due_date|
+        if due_date.include? search_term
+          print_list << [title, due_date]
+        end
+      end
+    elsif params[:date_exactly]
+      date = params[:date_exactly]
+      @@list.each do |title, due_date|
+        if due_date.include? date
+          print_list << [title, due_date]
+        end
+      end
+    end
+    print_list
+  end
+end
